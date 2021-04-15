93f993934221e0d56701c8a0f813ebe6e4599aa3
BRASS <mr.brn.lewis@outlook.com>
2021-04-15 19:41:49 +0800

    Good with promise and object manipulation now

--- Assets/script.js
+++ Assets/script.js
@@ -3,33 +3,22 @@
 let appID = "&appid=edf705e3020c7eb275d6596cf76f80e8";
 const weatherCall = "http://api.openweathermap.org/data/2.5/weather?q=";
 let city = "perth";
-let tempeture;
+let tempeture;``
 let lat;
 
 
-async function contactWeatherAPI(city)
+function contactWeatherAPI(city)
 {
 
-try 
-{
-    let report = await fetch(weatherCall + city + "&" + appID);
-    return await report.json();
-
-} 
-
-catch (error)
-{
-    console.log(error);
+fetch(weatherCall + city + "&" + appID)
+    .then(function response(data) {
+        return data.json();
+    })
+    .then(function process(pData) {
+        let i = document.getElementById("curTemp");
+        i.textContent = "Todays Weather is : " + pData.weather.description + " and " + pData.main.temp + " celcius";
+    });
 }
 
+contactWeatherAPI("Canberra");
 
-}
-
-async function createWeather()
-{
-    let test = await contactWeatherAPI("canberra");
-    let divShow = document.getElementById("show");
-    divShow.innerHTML = JSON.stringify(test.coord.lat)
-}
-
-createWeather();
