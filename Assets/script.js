52e156656d327fe4e764e4b6e07921515d6bea1b
BRASS <mr.brn.lewis@outlook.com>
2021-04-16 16:54:47 +0800

    styles and current weather setup

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,52 @@
 /* my appid key  */
 
-let appID = "&appid=edf705e3020c7eb275d6596cf76f80e8";
+const appID = "&appid=edf705e3020c7eb275d6596cf76f80e8";
 const weatherCall = "http://api.openweathermap.org/data/2.5/weather?q=";
-let city = "perth";
-let tempeture;``
-let lat;
+const wUnits = "&units=metric"
+const iconURL = "http://openweathermap.org/img/wn/";
+
+const currentTemp = document.getElementById("curTemp");
+const currentWind = document.getElementById("curWind");
+const currentHumidity = document.getElementById("curHumidity");
+const currentIcon = document.getElementById("curIcon");
+const currentDesc = document.getElementById("curDesc");
 
 
 function contactWeatherAPI(city)
 {
 
-fetch(weatherCall + city + "&" + appID)
+let tempeture;
+let windSpeed;
+let humidity;
+let avIndex; // not sure how to find yet
+let weatherIcon;
+let desc;
+
+// using the baseline promise syntax. 
+//Also played around with ASYNC and AWAIT which works just as well.
+
+fetch(weatherCall + city  + wUnits + appID)
     .then(function response(data) {
         return data.json();
     })
     .then(function process(pData) {
-        let i = document.getElementById("curTemp");
-        i.textContent = "Todays Weather is : " + pData.weather.description + " and " + pData.main.temp + " celcius";
+
+        console.log(pData);
+        tempeture = pData.main.temp;
+        windSpeed = pData.wind.speed;
+        humidity = pData.main.humidity;
+        desc = pData.weather[0].description;
+        weatherIcon = iconURL + pData.weather[0].icon + "@2x.png";
+        tempeture = tempeture.toFixed(1);
+
+        currentIcon.setAttribute("src",weatherIcon);
+        currentDesc.innerHTML = desc;
+        currentTemp.innerHTML = tempeture + "°" + "<span class='curC'>C</span>";
+        currentWind.innerHTML = "Wind Speed: " + windSpeed + "  meter/sec";
+        currentHumidity.innerHTML = "Humidity: " + humidity + "%";
+
     });
 }
 
-contactWeatherAPI("Canberra");
+contactWeatherAPI("perth");
 
